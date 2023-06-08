54d3440ef1f8dd2ec59885640f76a5e27d56ec10
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-06-08 14:28:57 +0200

    lazy load countries module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './shared/pages/home-page/home-page.component';
-import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent
   }, 
   {
@@ -13,8 +14,16 @@ const routes: Routes = [
     component: AboutPageComponent
   },
   {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then ( m => m.CountriesModule )
+  },
+  {
     path: '**',
-    redirectTo: 'home'
+    redirectTo: ''
   },
 ]
 
