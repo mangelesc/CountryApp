c7359f5788ece9a5a0d9caab3a25713b5534079b
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-06-09 11:32:02 +0200

    Country service

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -6,8 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor ( private countriesServices: CountriesService ) {}
+
   searchByCapital ( term:string ): void {
-    console.log(term);
+    this.countriesServices.searchCapital( term )
+      .subscribe( countries => {
+        this.countries = countries;
+      }); 
   }
   
 }
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) { }
+
+  searchCapital( term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`;
+
+    // Defino la petición, pero la ejecuto cuando tengo un .subscribe, donde se dispara el observable
+    return this.http.get<Country[]>(url);
+  }
+  
+}
