42edef01c590987c4872cdf9b61192e64996d6c9
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-06-12 14:22:40 +0200

    -info route

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,20 +11,57 @@ import { CountriesService } from '../../services/countries.service';
   ]
 })
 export class CountryPageComponent implements OnInit {
-  
+
+  public country?: Country;
+
   constructor( 
     private activatedRoute: ActivatedRoute,
-    private countriesService: CountriesService
+    private countriesService: CountriesService,
+    private router: Router
   ){}
-  
+
   ngOnInit(): void {
     this.activatedRoute.params
-    .subscribe( ({ id }) => {
-      this.countriesService.searchCountryByAlphaCode( id )
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ))
+      )
       .subscribe( country => {
-        console.log(country)
+        if ( !country ) return this.router.navigateByUrl('');
+        
+        return this.country = country;
+
       });
-    })
   }
 
 }
+  // switchMap_> recibe el valor anterior (params, y desestructuro el id), regresa un nuevo observable
+  // Ahora el subscribe está subscrito el resultado de ese nuevo observable ( country/resp )
+
+
+   // ngOnInit(): void {
+    // this.activatedRoute.params
+    // .subscribe( ({ id }) => {
+    //   this.countriesService.searchCountryByAlphaCode( id )
+    //   .subscribe( country => {
+    //     console.log(country)
+    //   });
+    // })
+    // Evitar "subscribe hell"
+
+
+    // Posible manera: 
+    // ngOnInit(): void {
+    //   this.activatedRoute.params
+    //   .subscribe( ({ id }) => {
+    //     this.searchCountry(id);
+    //   })
+
+    // }
+
+    // searchCountry( code: string) {
+    //   this.countriesService.searchCountryByAlphaCode( code )
+    //     .subscribe( country => {
+    //       console.log(country)
+    //     });
+    // }
+    // }
