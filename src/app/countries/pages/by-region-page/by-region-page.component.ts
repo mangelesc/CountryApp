ee03d3533f6b6439cc81d3949a510fc9c7a1a75d
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-06-13 19:04:00 +0200

    by region page - optimización

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,7 +13,9 @@ import { Country } from '../../interfaces/country';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   public isLoading: boolean = false;
+
 
   constructor ( private countriesServices: CountriesService ) {}
 
