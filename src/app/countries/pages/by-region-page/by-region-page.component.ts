0adf7f765af4260636cd7bc581ea29f66ecb8bfa
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-06-13 19:07:26 +0200

    by region page - optimización

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -14,13 +14,17 @@ type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
+  
   public isLoading: boolean = false;
 
 
   constructor ( private countriesServices: CountriesService ) {}
 
-  searchByRegion ( term:string ): void {
+  searchByRegion ( term: Region ): void {
     this.isLoading = true;
+
+    this.selectedRegion = term;
     
     this.countriesServices.searchRegion( term )
       .subscribe( countries => {
